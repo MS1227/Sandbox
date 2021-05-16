c1baa922477ce5bb619b8360e1c3e52f433a0d2c
Matt Schnider <matt.schnider@gmail.com>
2021-05-16 15:08:54 -0400

    proof of concept of a json export option

--- cppSandbox.cpp
+++ cppSandbox.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <iomanip>
 #include <fstream>
+#include <sstream>
 
 enum class EventCode
 { System = 0x20000, SystemStart = 0x20001, StateChange = 0x30000, };
@@ -40,12 +41,30 @@ struct EventData
         threadId{ threadId },
         payload{ payload }
     {}
+
+    std::stringstream Tokenize(std::time_t time, const std::string& levelString)
+    {
+        std::stringstream tokens;
+        
+        tokens << "{\n";
+        tokens << "\t\"time\" : " << "\"" << std::put_time(std::gmtime(&time), "%c %Z") << "\",\n";
+        tokens << "\t\"level_string\" : " << "\"" << levelString << "\",\n";
+        tokens << "\t\"file\" : " << "\"" << file << "\",\n";
+        tokens << "\t\"function\" : " << "\"" << function << "\",\n";
+        tokens << "\t\"line\" : " << "\"" << line << "\",\n";
+        tokens << "\t\"thread_id\" : " << "\"" << threadId << "\",\n";
+        tokens << "\t\"payload\" : " << "\"" << payload << "\"\n";
+        tokens << "}\n";
+
+        return tokens;       
+    }
+
 };
 void LogMessage(EventCode code, EventData data)
 {
     std::map<EventCode, SeverityLevel> eventSeverityDictionary;
     eventSeverityDictionary[EventCode::SystemStart] = SeverityLevel::Info;
-   
+  
     std::time_t loggedTime = std::chrono::system_clock::to_time_t(data.eventTime);
 
     const auto level = eventSeverityDictionary.find(code);
@@ -61,6 +80,31 @@ void LogMessage(EventCode code, EventData data)
         default:
             break;
     }
+}
+
+void LogMessageInJSON(EventCode code, EventData data)
+{
+    std::fstream outfile("logFile.json", std::ios::out | std::ios::app);
+    std::map<EventCode, SeverityLevel> eventSeverityDictionary;
+    eventSeverityDictionary[EventCode::SystemStart] = SeverityLevel::Info;
+   
+    std::time_t loggedTime = std::chrono::system_clock::to_time_t(data.eventTime);
+
+    const auto level = eventSeverityDictionary.find(code);
+    const std::string infoString = "[INFO]";    
+
+    switch (level->second)
+    {
+        case SeverityLevel::Debug:
+            outfile << "[DEBUG] " << std::put_time(std::gmtime(&loggedTime), "%c %Z") << " - " << data;
+            break;
+        case SeverityLevel::Info:
+            outfile  << data.Tokenize(loggedTime, infoString).str();
+            break;
+        default:
+            break;
+    }
+
 }
 
 void LogMessageFile(EventCode code, EventData data)
@@ -90,6 +134,7 @@ void start()
 {
     EventData data(std::chrono::system_clock::now(),__FILE__, __FUNCTION__, std::to_string(__LINE__), std::this_thread::get_id(), "System starting...");
     LogMessage(EventCode::SystemStart, data);
+    LogMessageInJSON(EventCode::SystemStart, data);
     LogMessageFile(EventCode::SystemStart, data);
 }
 int main()
